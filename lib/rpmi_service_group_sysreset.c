0e3817b171f49d72eab560fa9f7acdf3088dacc7
Rahul Pathak <rpathak@ventanamicro.com>
2024-11-06 09:20:46 +0530

    lib: Correct the error code and service name in sysreset group
    As per the RPMI spec the return error code when the reset
    type is invalid is RPMI_ERR_INVALID_PARAM.
    Change the name of the service for getting the attributes.
    
    Also, define reset types as an enum like defined in other
    software layers. Also synchronize other services enum
    names.
    
    Signed-off-by: Rahul Pathak <rpathak@ventanamicro.com>

--- lib/rpmi_service_group_sysreset.c
+++ lib/rpmi_service_group_sysreset.c
@@ -48,16 +48,16 @@ static enum rpmi_error rpmi_sysreset_get_attributes(struct rpmi_service_group *g
 	struct rpmi_sysreset_group *sgrst = group->priv;
 	rpmi_uint32_t *resp = (void *)response_data;
 	const rpmi_uint32_t *sysreset_type;
-	rpmi_uint32_t reset_type, ret;
+	rpmi_uint32_t reset_type, attr = 0;
 
 	reset_type = rpmi_to_xe32(trans->is_be, ((const rpmi_uint32_t *)request_data)[0]);
 
 	sysreset_type = rpmi_sysreset_find_type(sgrst, reset_type);
-	ret = sysreset_type ? RPMI_SYSRST_ATTRIBUTES_FLAGS_SUPPORTED : 0;
+	attr = sysreset_type ? RPMI_SYSRST_ATTRS_FLAGS_RESETTYPE : 0;
 
 	*response_datalen = 2 * sizeof(*resp);
 	resp[0] = rpmi_to_xe32(trans->is_be, (rpmi_uint32_t)RPMI_SUCCESS);
-	resp[1] = rpmi_to_xe32(trans->is_be, ret);
+	resp[1] = rpmi_to_xe32(trans->is_be, attr);
 
 	return RPMI_SUCCESS;
 }
@@ -80,23 +80,25 @@ static enum rpmi_error rpmi_sysreset_do_reset(struct rpmi_service_group *group,
 	sysreset_type = rpmi_sysreset_find_type(sgrst, reset_type);
 	if (sysreset_type) {
 		DPRINTF("%s: Entering platform sysreset ..\n", __func__);
+		/* No returning back after this function */
 		sgrst->ops->do_system_reset(sgrst->ops_priv, reset_type);
 	}
 
+	/* reset_type is invalid at this point */
 	*response_datalen = sizeof(*resp);
-	resp[0] = rpmi_to_xe32(trans->is_be, (rpmi_uint32_t)RPMI_ERR_NOTSUPP);
+	resp[0] = rpmi_to_xe32(trans->is_be, (rpmi_uint32_t)RPMI_ERR_INVALID_PARAM);
 
 	return RPMI_SUCCESS;
 }
 
-static struct rpmi_service rpmi_sysreset_services[RPMI_SYSRST_SRV_MAX] = {
+static struct rpmi_service rpmi_sysreset_services[RPMI_SYSRST_SRV_ID_MAX] = {
 	[RPMI_SYSRST_SRV_ENABLE_NOTIFICATION] = {
 		.service_id = RPMI_SYSRST_SRV_ENABLE_NOTIFICATION,
 		.min_a2p_request_datalen = 4,
 		.process_a2p_request = NULL,
 	},
-	[RPMI_SYSRST_SRV_GET_SYSTEM_RESET_ATTRIBUTES] = {
-		.service_id = RPMI_SYSRST_SRV_GET_SYSTEM_RESET_ATTRIBUTES,
+	[RPMI_SYSRST_SRV_GET_ATTRIBUTES] = {
+		.service_id = RPMI_SYSRST_SRV_GET_ATTRIBUTES,
 		.min_a2p_request_datalen = 4,
 		.process_a2p_request = rpmi_sysreset_get_attributes,
 	},
@@ -138,7 +140,7 @@ rpmi_service_group_sysreset_create(rpmi_uint32_t sysreset_type_count,
 	group = &sgrst->group;
 	group->name = "sysreset";
 	group->servicegroup_id = RPMI_SRVGRP_SYSTEM_RESET;
-	group->max_service_id = RPMI_SYSRST_SRV_MAX;
+	group->max_service_id = RPMI_SYSRST_SRV_ID_MAX;
 	group->services = rpmi_sysreset_services;
 	group->lock = rpmi_env_alloc_lock();
 	group->priv = sgrst;
