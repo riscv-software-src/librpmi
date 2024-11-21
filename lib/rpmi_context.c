riscv-software-src/librpmi

commit 4a0137c00c32cb7750d58e81afc208d775fa2608

lib: Add context and service group privilege mode verification
    RPMI spec associated service groups with RISC-V privilege levels
    where they are accessible via RPMI transport.
    Add support for privilege level property in RPMI context
    and service group and verify if the service group
    is allowed to be added in an RPMI context based on the
    privilege level of the RPMI context

Signed-off-by: Rahul Pathak <rpathak@ventanamicro.com>

lib/rpmi_context.c
@@ -29,6 +29,9 @@ struct rpmi_context {
 	/** Current number of service groups in the context */
 	rpmi_uint32_t num_groups;
 
+	/** RISC-V privilege level of RPMI context */
+	enum rpmi_privilege_level privilege_level;
+
 	/** Current set of service groups in the context as an array */
 	struct rpmi_service_group **groups;
 
@@ -177,10 +180,17 @@ static enum rpmi_error rpmi_base_get_attributes(struct rpmi_service_group *group
 						rpmi_uint8_t *response_data)
 {
 	rpmi_uint32_t *resp = (void *)response_data;
+	struct rpmi_context *cntx = ((struct rpmi_base_group *)group->priv)->cntx;
+	rpmi_uint32_t flags = 0;
 
+	flags |= RPMI_BASE_FLAGS_F0_MSI_EN;
+	/* Set the context privilege level bit if context priv mode is M-mode */
+	flags |= (cntx->privilege_level == RPMI_PRIVILEGE_M_MODE_MASK) ?
+					RPMI_BASE_FLAGS_F0_PRIVILEGE : 0;
+	
 	*response_datalen = 5 * sizeof(*resp);
 	resp[0] = rpmi_to_xe32(trans->is_be, (rpmi_uint32_t)RPMI_SUCCESS);
-	resp[1] = rpmi_to_xe32(trans->is_be, RPMI_BASE_FLAGS_F0_MSI_EN);
+	resp[1] = rpmi_to_xe32(trans->is_be, flags);
 	resp[2] = 0;
 	resp[3] = 0;
 	resp[4] = 0;
@@ -292,6 +302,8 @@ static struct rpmi_service_group *rpmi_base_group_create(struct rpmi_context *cn
 	group = &base->group;
 	group->name = "base";
 	group->servicegroup_id = RPMI_SRVGRP_BASE;
+	/* Allowed for both M-mode and S-mode RPMI context */
+	group->privilege_level_bitmap = RPMI_PRIVILEGE_M_MODE_MASK | RPMI_PRIVILEGE_S_MODE_MASK;
 	group->max_service_id = RPMI_BASE_SRV_ID_MAX;
 	group->services = rpmi_base_services;
 	group->lock = rpmi_env_alloc_lock();
@@ -529,6 +541,18 @@ struct rpmi_service_group *rpmi_context_find_group(struct rpmi_context *cntx,
 	return ret;
 }
 
+static enum rpmi_error rpmi_context_verify_privilege_level(struct rpmi_context *cntx,
+						      struct rpmi_service_group *group)
+{
+	if (!group->privilege_level_bitmap)
+		return RPMI_ERR_INVALID_PARAM;
+
+	if (group->privilege_level_bitmap & (1U << cntx->privilege_level))
+		return RPMI_SUCCESS;
+
+	return RPMI_ERR_DENIED;
+}
+
 enum rpmi_error rpmi_context_add_group(struct rpmi_context *cntx,
 				       struct rpmi_service_group *group)
 {
@@ -557,6 +581,10 @@ enum rpmi_error rpmi_context_add_group(struct rpmi_context *cntx,
 			goto fail_unlock;
 		}
 	}
+
+	rc = rpmi_context_verify_privilege_level(cntx, group);
+	if (rc)
+		goto fail_unlock;
 
 	cntx->groups[cntx->num_groups] = group;
 	cntx->num_groups++;
@@ -597,13 +625,15 @@ void rpmi_context_remove_group(struct rpmi_context *cntx,
 struct rpmi_context *rpmi_context_create(const char *name,
 					 struct rpmi_transport *trans,
 					 rpmi_uint32_t max_num_groups,
+					 enum rpmi_privilege_level privilege_level,
 					 rpmi_uint32_t plat_info_len,
 					 const char *plat_info)
 {
 	struct rpmi_context *cntx;
 	enum rpmi_error rc;
 
-	if (!name || !trans || !max_num_groups) {
+	if (!name || !trans || !max_num_groups ||
+			privilege_level >= RPMI_PRIVILEGE_LEVEL_MAX_IDX) {
 		DPRINTF("%s: invalid parameters\n", __func__);
 		return NULL;
 	}
@@ -622,6 +652,7 @@ struct rpmi_context *rpmi_context_create(const char *name,
 	cntx->name = name;
 	cntx->trans = trans;
 	cntx->max_num_groups = max_num_groups;
+	cntx->privilege_level = privilege_level;
 
 	/**
 	 * Allocate for the array of pointers to the service

include/librpmi.h
@@ -169,6 +169,19 @@ enum rpmi_queue_type {
 	RPMI_QUEUE_MAX,
 };
 
+/**
+ * RISC-V privilege levels associated with
+ * RPMI context and service groups
+ */
+enum rpmi_privilege_level {
+	RPMI_PRIVILEGE_S_MODE = 0,
+	RPMI_PRIVILEGE_M_MODE = 1,
+	RPMI_PRIVILEGE_LEVEL_MAX_IDX,
+};
+
+#define RPMI_PRIVILEGE_S_MODE_MASK	(1U << RPMI_PRIVILEGE_S_MODE)
+#define RPMI_PRIVILEGE_M_MODE_MASK	(1U << RPMI_PRIVILEGE_M_MODE)
+
 /** RPMI ServiceGroups IDs */
 enum rpmi_servicegroup_id {
 	RPMI_SRVGRP_ID_MIN = 0,
@@ -639,6 +652,7 @@ void rpmi_context_remove_group(struct rpmi_context *cntx,
  * @param[in] name		name of the context instance
  * @param[in] trans		pointer to RPMI transport instance
  * @param[in] max_num_groups	maximum number of service groups
+ * @param[in] privilege_level	RISC-V privilege level of the RPMI context
  * @param[in] plat_info_len	length of the Platform info string
  * @param[in] plat_info		pointer to the Platform info string
  * @return pointer to RPMI context upon success and NULL upon failure
@@ -646,6 +660,7 @@ void rpmi_context_remove_group(struct rpmi_context *cntx,
 struct rpmi_context *rpmi_context_create(const char *name,
 					 struct rpmi_transport *trans,
 					 rpmi_uint32_t max_num_groups,
+					 enum rpmi_privilege_level privilege_level,
 					 rpmi_uint32_t plat_info_len,
 					 const char *plat_info);
 
@@ -701,6 +716,14 @@ struct rpmi_service_group {
 
 	/** Maximum service ID of the service group */
 	rpmi_uint8_t max_service_id;
+
+	/**
+	 * RISC-V privilagel level bitmap where this group
+	 * is allowed to be accessible. enum rpmi_privilege_level
+	 * values represents the bit positions which if are
+	 * set, the access to that privilegel level is enabled
+	 */
+	rpmi_uint32_t privilege_level_bitmap;
 
 	/** Array of services indexed by service ID */
 	struct rpmi_service *services;

lib/rpmi_service_group_clock.c
@@ -857,6 +857,8 @@ rpmi_service_group_clock_create(rpmi_uint32_t clock_count,
 	group = &clkgrp->group;
 	group->name = "clk";
 	group->servicegroup_id = RPMI_SRVGRP_CLOCK;
+	/* Allowed for both M-mode and S-mode RPMI context */
+	group->privilege_level_bitmap = RPMI_PRIVILEGE_M_MODE_MASK | RPMI_PRIVILEGE_S_MODE_MASK;
 	group->max_service_id = RPMI_CLK_SRV_ID_MAX;
 	group->services = rpmi_clock_services;
 	group->lock = rpmi_env_alloc_lock();

lib/rpmi_service_group_cppc.c
@@ -931,6 +931,8 @@ rpmi_service_group_cppc_create(struct rpmi_hsm *hsm,
 	group = &cppcgrp->group;
 	group->name = "cppc";
 	group->servicegroup_id = RPMI_SRVGRP_CPPC;
+	/* Allowed for both M-mode and S-mode RPMI context */
+	group->privilege_level_bitmap = RPMI_PRIVILEGE_M_MODE_MASK | RPMI_PRIVILEGE_S_MODE_MASK;
 	group->max_service_id = RPMI_CPPC_SRV_ID_MAX;
 	group->services = rpmi_cppc_services;
 	group->process_events = rpmi_cppc_process_events;

lib/rpmi_service_group_hsm.c
@@ -332,6 +332,8 @@ struct rpmi_service_group *rpmi_service_group_hsm_create(struct rpmi_hsm *hsm)
 	group = &sghsm->group;
 	group->name = "hsm";
 	group->servicegroup_id = RPMI_SRVGRP_HSM;
+	/* Allowed only for M-mode RPMI context */
+	group->privilege_level_bitmap = RPMI_PRIVILEGE_M_MODE_MASK;
 	group->max_service_id = RPMI_HSM_SRV_ID_MAX;
 	group->services = rpmi_hsm_services;
 	group->process_events = rpmi_hsm_process_events;

lib/rpmi_service_group_sysreset.c
@@ -140,6 +140,8 @@ rpmi_service_group_sysreset_create(rpmi_uint32_t sysreset_type_count,
 	group = &sgrst->group;
 	group->name = "sysreset";
 	group->servicegroup_id = RPMI_SRVGRP_SYSTEM_RESET;
+	/* Allowed only for M-mode RPMI context */
+	group->privilege_level_bitmap = RPMI_PRIVILEGE_M_MODE_MASK;
 	group->max_service_id = RPMI_SYSRST_SRV_ID_MAX;
 	group->services = rpmi_sysreset_services;
 	group->lock = rpmi_env_alloc_lock();

lib/rpmi_service_group_syssusp.c
@@ -244,6 +244,8 @@ rpmi_service_group_syssusp_create(struct rpmi_hsm *hsm,
 	group = &sgsusp->group;
 	group->name = "syssusp";
 	group->servicegroup_id = RPMI_SRVGRP_SYSTEM_SUSPEND;
+	/* Allowed only for M-mode RPMI context */
+	group->privilege_level_bitmap = RPMI_PRIVILEGE_M_MODE_MASK;
 	group->max_service_id = RPMI_SYSSUSP_SRV_ID_MAX;
 	group->services = rpmi_syssusp_services;
 	group->process_events = rpmi_syssusp_process_events;

test/test_common.c
@@ -252,6 +252,7 @@ int test_scenario_default_init(struct rpmi_test_scenario *scene)
 
 	scene->cntx = rpmi_context_create("test_context", scene->xport,
 					  scene->max_num_groups,
+					  RPMI_PRIVILEGE_M_MODE,
 					  scene->base.plat_info_len, scene->base.plat_info);
 	if (!scene->cntx) {
 		printf("%s: failed to create test rpmi_context\n ", __func__);

commit 5bf59a51e5a1e92b8c9eaf3c21a999f84ddf654f

lib: Fix priv-level comparison in rpmi_base_get_attributes()
Compare the actual priv-level in rpmi_base_get_attributes() instead
of priv-level mask.

Fixes: 4a0137c00c32 ("lib: Add context and service group privilege mode verification")
Signed-off-by: Anup Patel <apatel@ventanamicro.com>

lib/rpmi_context.c
@@ -185,9 +185,9 @@ static enum rpmi_error rpmi_base_get_attributes(struct rpmi_service_group *group
 
 	flags |= RPMI_BASE_FLAGS_F0_MSI_EN;
 	/* Set the context privilege level bit if context priv mode is M-mode */
-	flags |= (cntx->privilege_level == RPMI_PRIVILEGE_M_MODE_MASK) ?
+	flags |= (cntx->privilege_level == RPMI_PRIVILEGE_M_MODE) ?
 					RPMI_BASE_FLAGS_F0_PRIVILEGE : 0;
-	
+
 	*response_datalen = 5 * sizeof(*resp);
 	resp[0] = rpmi_to_xe32(trans->is_be, (rpmi_uint32_t)RPMI_SUCCESS);
 	resp[1] = rpmi_to_xe32(trans->is_be, flags);