5751aeec52f3b574b912b41ac2b5889042ddb644
Carlos López <carlos.lopezr4096@gmail.com>
2025-11-24 10:40:32 +0100

    lib: performance: fix memory leaks in group creation error path
    Do not forget to clean up previous allocations if something fails during
    in rpmi_service_group_perf_create().
    
    Signed-off-by: Carlos López <carlos.lopezr4096@gmail.com>

--- lib/rpmi_service_group_performance.c
+++ lib/rpmi_service_group_performance.c
@@ -761,6 +761,7 @@ rpmi_service_group_perf_create(rpmi_uint32_t perf_count,
 	if (!perfgrp->fc_memory_region) {
 		DPRINTF("%s: failed to allocate perf service group instance\n",
 			__func__);
+		rpmi_env_free(perfgrp);
 		return NULL;
 	}
 
@@ -770,6 +771,7 @@ rpmi_service_group_perf_create(rpmi_uint32_t perf_count,
 						 ops_priv);
 	if (!perfgrp->perf_tree) {
 		DPRINTF("%s: failed to initialize perf tree\n", __func__);
+		rpmi_env_free(perfgrp->fc_memory_region);
 		rpmi_env_free(perfgrp);
 		return NULL;
 	}
