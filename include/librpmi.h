ca80774822c3b3562e4a0835034c956ab20b22d7
Subrahmanya Lingappa <subrahmanya.lingappa@qti.qualcomm.com>
2026-08-20 12:20:19 +0530

    lib: align logging service with LOG_DATA
    Update LOGGING service 0x02 to match the LOG_DATA request format
    defined by the RPMI specification.
    
    Rename the public service ID and parse LOG_TYPE, NUM_WORDS, and
    WORDS[] with validation before forwarding decoded words to the
    platform callback.
    
    Keep notification enablement explicitly unsupported, allow the group
    from both S-mode and M-mode, and release the group lock on destroy.
    
    Signed-off-by: Subrahmanya Lingappa <subrahmanya.lingappa@qti.qualcomm.com>

--- include/librpmi.h
+++ include/librpmi.h
@@ -198,7 +198,7 @@ enum rpmi_servicegroup_id {
 	RPMI_SRVGRP_MANAGEMENT_MODE	= 0x000B,
 	RPMI_SRVGRP_RAS_AGENT		= 0x000C,
 	RPMI_SRVGRP_REQUEST_FORWARD	= 0x000D,
-	RPMI_SRVGRP_LOGGING			= 0x000E,
+	RPMI_SRVGRP_LOGGING		= 0x000E,
 	RPMI_SRVGRP_ID_MAX_COUNT,
 
 	/* Reserved range for service groups */
@@ -373,7 +373,7 @@ enum rpmi_mm_service_id {
 /** RPMI LOGGING ServiceGroup Service IDs */
 enum rpmi_logging_service_id {
 	RPMI_LOGGING_SRV_ENABLE_NOTIFICATION = 0x01,
-	RPMI_LOGGING_SRV_SET_CONFIG = 0x02,
+	RPMI_LOGGING_SRV_LOG_DATA = 0x02,
 	RPMI_LOGGING_SRV_ID_MAX,
 };
 
@@ -2132,9 +2132,9 @@ enum rpmi_error rpmi_mm_service_register(struct rpmi_service_group *group,
 
 /** Platform specific logging operations */
 struct rpmi_logging_platform_ops {
-	enum rpmi_error (*do_set_state)(void *priv, rpmi_uint32_t log_type,
-					rpmi_uint32_t datalen_bytes,
-					const void *data);
+	enum rpmi_error (*log_data)(void *priv, rpmi_uint32_t log_type,
+				    rpmi_uint32_t num_words,
+				    const rpmi_uint32_t *words);
 };
 
 /**
--- lib/rpmi_service_group_logging.c
+++ lib/rpmi_service_group_logging.c
@@ -20,61 +20,82 @@ struct rpmi_logging_group {
 };
 
 static enum rpmi_error
-rpmi_logging_set_state(struct rpmi_logging_group *sglogging,
-		      rpmi_uint32_t log_type, rpmi_uint32_t datalen_bytes,
-		      const void *data)
+rpmi_logging_log_data(struct rpmi_logging_group *sglogging,
+		      rpmi_uint32_t log_type, rpmi_uint32_t num_words,
+		      const rpmi_uint32_t *words)
 {
 	enum rpmi_error ret;
 
-	ret = sglogging->ops->do_set_state(sglogging->ops_priv, log_type,
-					   datalen_bytes, data);
+	ret = sglogging->ops->log_data(sglogging->ops_priv, log_type,
+				       num_words, words);
 
 	return ret;
 }
 
 static enum rpmi_error
-rpmi_logging_sg_set_config(struct rpmi_service_group *group,
-			   struct rpmi_service *service,
-			   struct rpmi_transport *trans,
-			   rpmi_uint16_t request_datalen,
-			   const rpmi_uint8_t *request_data,
-			   rpmi_uint16_t *response_datalen,
-			   rpmi_uint8_t *response_data)
+rpmi_logging_sg_log_data(struct rpmi_service_group *group,
+			 struct rpmi_service *service,
+			 struct rpmi_transport *trans,
+			 rpmi_uint16_t request_datalen,
+			 const rpmi_uint8_t *request_data,
+			 rpmi_uint16_t *response_datalen,
+			 rpmi_uint8_t *response_data)
 {
-	enum rpmi_error status;
+	const rpmi_uint32_t *req = (const rpmi_uint32_t *)request_data;
+	rpmi_uint32_t log_type, num_words, remaining_words, i;
 	struct rpmi_logging_group *logginggrp = group->priv;
 	rpmi_uint32_t *resp = (void *)response_data;
-	rpmi_uint32_t log_type;
-	const rpmi_uint8_t *data;
-	rpmi_uint32_t datalen_bytes;
+	rpmi_uint32_t *words = NULL;
+	enum rpmi_error status;
 
-	if (request_datalen < sizeof(rpmi_uint32_t)) {
-		resp[0] = rpmi_to_xe32(trans->is_be,
-				       (rpmi_uint32_t)RPMI_ERR_INVALID_PARAM);
-		*response_datalen = sizeof(*resp);
-		return RPMI_SUCCESS;
+	if (request_datalen < 2 * sizeof(*req) ||
+	    (request_datalen - (2 * sizeof(*req))) % sizeof(*req)) {
+		status = RPMI_ERR_INVALID_PARAM;
+		goto done;
 	}
 
-	log_type = rpmi_to_xe32(trans->is_be,
-				((const rpmi_uint32_t *)request_data)[0]);
-	data = request_data + sizeof(log_type);
-	datalen_bytes = request_datalen - sizeof(log_type);
+	log_type = rpmi_to_xe32(trans->is_be, req[0]);
+	num_words = rpmi_to_xe32(trans->is_be, req[1]);
+	remaining_words = (request_datalen - (2 * sizeof(*req))) / sizeof(*req);
 
-	/* change logging config synchronously */
-	status = rpmi_logging_set_state(logginggrp, log_type, datalen_bytes,
-					data);
+	if (num_words != remaining_words) {
+		status = RPMI_ERR_INVALID_PARAM;
+		goto done;
+	}
+
+	if (num_words) {
+		words = rpmi_env_zalloc(num_words * sizeof(*words));
+		if (!words) {
+			status = RPMI_ERR_FAILED;
+			goto done;
+		}
+
+		for (i = 0; i < num_words; i++)
+			words[i] = rpmi_to_xe32(trans->is_be, req[2 + i]);
+	}
+
+	status = rpmi_logging_log_data(logginggrp, log_type, num_words, words);
+
+	if (words)
+		rpmi_env_free(words);
+
+done:
 	resp[0] = rpmi_to_xe32(trans->is_be, (rpmi_uint32_t)status);
-
 	*response_datalen = sizeof(*resp);
 
 	return RPMI_SUCCESS;
 }
 
 static struct rpmi_service rpmi_logging_services[RPMI_LOGGING_SRV_ID_MAX] = {
-	[RPMI_LOGGING_SRV_SET_CONFIG] = {
-		.service_id = RPMI_LOGGING_SRV_SET_CONFIG,
-		.min_a2p_request_datalen = 16,
-		.process_a2p_request = rpmi_logging_sg_set_config,
+	[RPMI_LOGGING_SRV_ENABLE_NOTIFICATION] = {
+		.service_id = RPMI_LOGGING_SRV_ENABLE_NOTIFICATION,
+		.min_a2p_request_datalen = 2 * sizeof(rpmi_uint32_t),
+		.process_a2p_request = NULL,
+	},
+	[RPMI_LOGGING_SRV_LOG_DATA] = {
+		.service_id = RPMI_LOGGING_SRV_LOG_DATA,
+		.min_a2p_request_datalen = 2 * sizeof(rpmi_uint32_t),
+		.process_a2p_request = rpmi_logging_sg_log_data,
 	},
 };
 
@@ -86,7 +107,7 @@ struct rpmi_service_group *rpmi_service_group_logging_create(
 	struct rpmi_logging_group *sglogging;
 
 	/* All critical parameters should be non-NULL */
-	if (!ops || !ops->do_set_state) {
+	if (!ops || !ops->log_data) {
 		DPRINTF("%s: invalid parameters\n", __func__);
 		return NULL;
 	}
@@ -108,8 +129,9 @@ struct rpmi_service_group *rpmi_service_group_logging_create(
 	group->servicegroup_version =
 		RPMI_BASE_VERSION(RPMI_SPEC_VERSION_MAJOR,
 				  RPMI_SPEC_VERSION_MINOR);
-	/* Allowed only for M-mode RPMI context */
-	group->privilege_level_bitmap = RPMI_PRIVILEGE_M_MODE_MASK;
+	/* Allowed for both M-mode and S-mode RPMI context */
+	group->privilege_level_bitmap =
+		RPMI_PRIVILEGE_M_MODE_MASK | RPMI_PRIVILEGE_S_MODE_MASK;
 	group->max_service_id = RPMI_LOGGING_SRV_ID_MAX;
 	group->services = rpmi_logging_services;
 	group->lock = rpmi_env_alloc_lock();
@@ -125,5 +147,6 @@ void rpmi_service_group_logging_destroy(struct rpmi_service_group *group)
 		return;
 	}
 
+	rpmi_env_free_lock(group->lock);
 	rpmi_env_free(group->priv);
 }
